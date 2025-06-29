940afdbea6d21b2ba927622478effa61cc386691
Naveen Nagar <naveennagar9617@gmail.com>
2025-06-29 10:00:00 +0530

    Initial commit: Spring Boot project setup with Maven

---
+++ backend/src/main/java/com/fixmate/FixMateApplication.java
@@ -0,0 +1,14 @@
+package com.fixmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class FixMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixMateApplication.class, args);
+    }
+}
